a005b51c44c3947b6b6d87fccd6483d62915558b
PunyTitan <pinjiahe@gmail.com>
2015-08-19 14:23:03 +0800

    1032 completed

--- Bailian/1032.cpp
+++ Bailian/1032.cpp
@@ -6,75 +6,45 @@
 
 using namespace std;
 
-int getFactor(int number)
-{
-	if(number <= 4)
-		return -1;
-	if(number == 6)
-		return 3;
-	for(int i = 2; i<=sqrt(number); ++i)
-	{
-		if(number%i == 0)
-			return i;
-	}
-
-	return -1;
-}
-
 int main(int argc, char const *argv[])
 {
-	int N, current_factor, factor1, factor2, indicator;
-	queue<int> factors;
-	vector<int> results;
-
+	vector<int> result;
+	int currentSum = 0;
+	int N;
+	int leftover;
 	cin>>N;
-	factors.push(N);
 
-	while(!factors.empty())
+	for(int i=2; ; ++i)
 	{
-		current_factor = factors.front();
-		factors.pop();
-
-		indicator = getFactor(current_factor);
-
-		if(indicator == -1)
+		currentSum += i;
+		if(currentSum == N)
 		{
-			if(current_factor >= 2 && current_factor <= 4)
-				results.push_back(current_factor);
-			else 
+			result.push_back(i);
+			break;
+		}
+			
+		if(currentSum > N)
+		{
+			leftover = N-(currentSum-i);
+			if(leftover/result.size() == 1)
 			{
-				if(current_factor%2 == 0)
-					factor1 = factor2 = current_factor/2;
-				else
-				{
-					factor1 = current_factor/2;
-					factor2 = current_factor/2+1;
-				}
-				factors.push(factor1);
-				factors.push(factor2);
+				for(vector<int>::iterator itr=result.begin(); itr!=result.end(); ++itr)
+					*itr += 1;
+				leftover -= result.size();
 			}
+			for(vector<int>::iterator itr=--result.end(); leftover!=0; --leftover)
+			{
+				++(*itr);
+				--itr;
+			}
+			break;	
 		}
-		else if(indicator == 2)
-		{	
-			for(int i=0; i<current_factor/indicator/2; ++i)
-				results.push_back(4);
-			if(current_factor/indicator%2 == 1)
-				results.push_back(2);
-				
-		}
-		else
-		{
-			for(int i=0; i<current_factor/indicator; ++i)
-				results.push_back(indicator);
-		}
-		
+
+		result.push_back(i);
 	}
 
-	sort(results.begin(), results.end());
-
-	for(vector<int>::const_iterator itr = results.begin(); itr!=results.end(); ++itr)
+	for(vector<int>::const_iterator itr=result.begin(); itr!=result.end(); ++itr)
 		cout<<*itr<<" ";
-
 
 	return 0;
 }
