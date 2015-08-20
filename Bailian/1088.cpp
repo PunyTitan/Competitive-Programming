5da02ff8b98812e367488fbf455783b4fc366f30
PunyTitan <pinjiahe@gmail.com>
2015-08-20 19:19:19 +0800

    draft

--- Bailian/1088.cpp
+++ Bailian/1088.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int pathLength(vector< vector<int> > & m, int row, int col)
 {
-
+	int left, right, up, down;
+	if(row == 0 || m[row-1][col]>m[row][col])
+		up = 0;
+	else
+		up = pathLength(m, )
 }
 
 int main(int argc, char const *argv[])
@@ -13,6 +17,7 @@ int main(int argc, char const *argv[])
 	vector< vector<int> > skiField;
 	vector< vector<int> > flagV;
 	int row, col;
+	int largest = -1;
 
 	cin>>row>>col;
 
@@ -32,7 +37,17 @@ int main(int argc, char const *argv[])
 		for(int j=0; j<col; ++j)
 			cin>>skiField[i][j];
 
-	
+	for(int i=0; i<row; ++i)
+		for(int j=0; j<col; ++j)
+			if(flagV[i][j] == -1)
+				skiField[i][j] = pathLength(skiField, i, j);
+
+	for(int i=0; i<row; ++i)
+		for(int j=0; j<col; ++j)
+			if(flagV[i][j]>largest)
+				largest = flagV[i][j];
+
+	cout<<largest<<endl;
 
 	return 0;
 }
