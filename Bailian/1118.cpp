8d660d2be73770c027e1e9acd82960c8befb1190
PunyTitan <pinjiahe@gmail.com>
2015-08-21 17:38:57 +0800

    WA

--- Bailian/1118.cpp
+++ Bailian/1118.cpp
@@ -3,60 +3,108 @@
 #include <map>
 #include <limits>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
-
-void calculateLine(pair<int, int> &point1, pair<int, int> &point2, double &k, double &b)
+class Point
 {
-	if (point1.first == point2.first)
+public:
+	Point(int x = 0, int y = 0) :x(x), y(y)
+	{}
+
+	int x;
+	int y;
+
+};
+
+double getSlope(Point &point1, Point &point2)
+{
+	double k;
+	k = 1.0 * (point1.y - point2.y) / (point1.x - point2.x);
+
+	return k;
+}
+
+int getMax(vector<double> & slopes)
+{
+	int maximum = 0;
+	int count = 0;
+	double lastSlop = slopes[0];
+
+	for (vector<double>::const_iterator itr = slopes.begin(); itr != slopes.end(); ++itr)
 	{
-		k = numeric_limits<double>::infinity();
-		b = numeric_limits<double>::infinity();
-		return;
+		if (*itr == lastSlop)
+			++count;
+		else
+		{
+			if (count>maximum)
+				maximum = count;
+			count = 1;
+			lastSlop = *itr;
+		}
 	}
-	k = 1.0 * (point1.second - point2.second) / (point1.first - point2.first);
-	b = point1.second - point1.first * k;
+
+	if (count > maximum)
+		maximum = count;
+
+	return maximum + 1;
 }
 
 int main(int argc, char const *argv[])
 {
-	map< pair<double, double>, set<int> > lines;
-	vector< pair<int, int> > points(700);
-	int numOfNode, x, y, largest = 0;
-	double k, b;
-	k = b = 0;
+	vector< Point > points;
+	vector<double> slopes;
+	int numOfPoints, x, y, temp, largest = 0;
+	int vertical;
 
-	cin >> numOfNode;
-	while (numOfNode != 0)
+	cin >> numOfPoints;
+	while (numOfPoints != 0)
 	{
-		for (int i = 0; i<numOfNode; ++i)
+		for (int i = 0; i<numOfPoints; ++i)
 		{
 			cin >> x >> y;
-			points[i].first = x;
-			points[i].second = y;
+			points.push_back(Point(x, y));
 		}
 
-		
-
-		for (int i = 0; i + 1<numOfNode; ++i)
-		for (int j = i + 1; j<numOfNode; ++j)
+		for (int i = 0; i + 1<numOfPoints; ++i)
 		{
-			//cout << "compare: (" << points[i].first << "," << points[i].second << ") & (" << points[j].first << "," << points[j].second<<")" << endl;
-			calculateLine(points[i], points[j], k, b);
-			//cout << "k: " << k << "  b: " << b << endl;
-			lines[pair<double, double>(k, b)].insert(j);
-			lines[pair<double, double>(k, b)].insert(i);
+			if (largest >= numOfPoints - i)
+				break;
+
+			vertical = 1;
+			for (int j = i+1; j < numOfPoints; ++j)
+			{
+				if (points[j].x == points[i].x)
+					++vertical;
+				else
+					slopes.push_back(getSlope(points[i], points[j]));
+			}
+
+			
+
+			if (vertical>largest)
+				largest = vertical;
+			if (slopes.size()+1 > largest)
+			{
+				//cout << "here" << endl;
+				sort(slopes.begin(), slopes.end());
+				for(vector<int>::const_iterator itr = slopes.begin(); itr != slopes.end(); ++itr)
+					cout<<*itr<<" ";
+				temp = getMax(slopes);
+				//cout << "temp" << temp << endl;
+				if (temp>largest)
+					largest = temp;				
+			}
+			slopes.clear();
 			
 		}
 
-		for (map< pair<double, double>, set<int> >::const_iterator itr = lines.begin(); itr != lines.end(); ++itr)
-		if ((itr->second).size()>largest)
-			largest = (itr->second).size();
-
 		cout << largest << endl;
-		cin >> numOfNode;
-
+		points.clear();
+		cin >> numOfPoints;
 	}
+
+
 	return 0;
 }
