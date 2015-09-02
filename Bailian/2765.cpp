a2cc976e0ab810673fcc68a03ca5bd8de2b218df
PunyTitan <pinjiahe@gmail.com>
2015-09-02 08:43:51 +0800

    2765 completed
    last WA was caused by "MAX_LENGTH", 30 is not large enough to keep high
    precision.

--- Bailian/2765.cpp
+++ Bailian/2765.cpp
@@ -5,8 +5,9 @@
 
 using namespace std;
 
-const int MAX_LENGTH = 30;
+const int MAX_LENGTH = 50;
 
+//add two numbers' decimal digits
 vector<int> addVec(vector<int> data1, vector<int> data2)
 {
 	int extra = 0;
@@ -23,6 +24,7 @@ vector<int> addVec(vector<int> data1, vector<int> data2)
 	return result;
 }
 
+//convert a double variable to vector<int> to keep high precision
 vector<int> doubleToVec(double number)
 {
 	vector<int> result(MAX_LENGTH);
@@ -36,19 +38,22 @@ vector<int> doubleToVec(double number)
 	return result;
 }
 
+
 int main(int argc, char const *argv[])
 {
 	string oct;
 	int digit;
-	vector<int> dec(30);
+	vector<int> dec(MAX_LENGTH);
 
 	while(cin>>oct)
 	{
 		digit = 1;
-		dec.resize(30);
+		dec.resize(MAX_LENGTH);
 		fill(dec.begin(), dec.end(), 0);
+		
 		for(int i=2; i<oct.length(); ++i)
 		{
+			//this statement can be more efficient by using reference in addVec
 			dec = addVec(dec, doubleToVec(pow(1.0/8, digit)*(oct[i]-'0')));
 			++digit;
 		}
@@ -60,9 +65,15 @@ int main(int argc, char const *argv[])
 			dec.pop_back();
 		}
 
+
 		cout<<oct<<" [8] = 0.";
-		for(int i=0; i<dec.size(); ++i)
-			cout<<dec[i];
+		if(dec.size()==0)
+			cout<<0;
+		else
+		{
+			for(int i=0; i<dec.size(); ++i)
+				cout<<dec[i];
+		}		
 		cout<<" [10]"<<endl;
 	}	
 	return 0;
