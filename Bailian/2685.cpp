db37dc8463dcf02fbae0b769ae40372d29f52bd7
PunyTitan <pinjiahe@gmail.com>
2015-09-07 16:46:29 +0800

    2685 completed

---
+++ Bailian/2685.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int sum;
+	int number;
+
+	for(int i=100; i<=999; ++i)
+	{
+		sum = 0;
+		number = i;
+		while(number!=0)
+		{
+			sum += (int)pow(number%10, 3);
+			number /= 10;
+		}
+	}
+	return 0;
+}
