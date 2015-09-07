f54c5fb7a3df5619550d1c0fa6ce436be2f61dce
PunyTitan <pinjiahe@gmail.com>
2015-09-07 22:52:49 +0800

    2682 completed

---
+++ Bailian/2682.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int n, m;
+	int temp;
+	int *data;
+
+	cin>>n>>m;
+	data = new int[n];
+
+	for(int i=0; i<n; ++i)
+		cin>>data[i];
+
+	for(int i=0; i<m; ++i)
+	{
+		temp = data[n-1];
+		for(int j=0; j<n-1; ++i)
+		{
+			data[n-1-j] = data[n-2-j];
+		}
+		data[0] = temp;
+	}
+
+	for(int i=0; i<n; ++i)
+		cout<<data[i]<<" ";
+	return 0;
+}
