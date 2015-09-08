8bba484d65153f07d930b963e0ea4dca46236f68
PunyTitan <pinjiahe@gmail.com>
2015-09-08 13:14:47 +0800

    2797 completed

---
+++ Bailian/2797.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	vector<string> strs;
+	string current_str, subStr;
+
+	while (cin >> current_str)
+	{
+		strs.push_back(current_str);
+	}
+
+	for (int i = 0; i<strs.size(); ++i)
+	{
+		cout << strs[i] << " ";
+		for (int j = 1; j <= strs[i].length(); ++j)
+		{
+			bool notMatch = true;
+			subStr = strs[i].substr(0, j);
+
+			for (int k = 0; k<strs.size(); ++k)
+			{
+				if (i == k)
+					continue;
+
+				if (subStr == strs[k].substr(0, j))
+				{
+					notMatch = false;
+					break;
+				}
+
+			}
+
+			if (notMatch)
+			{
+				cout << subStr << endl;
+				break;
+			}
+		}
+		if(!notMatch)
+			cout << strs[i] << endl;
+	}
+	return 0;
+}
