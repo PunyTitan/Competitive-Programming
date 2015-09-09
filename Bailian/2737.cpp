898eec6d6e280fde019ddc135bbfa1bcb490a7f4
PunyTitan <pinjiahe@gmail.com>
2015-09-09 23:21:47 +0800

    WA

--- Bailian/2737.cpp
+++ Bailian/2737.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdio>
 
 using namespace std;
 
 void strMinus(string & str1, string & str2)
 {
 	int next = 0;
+
+
+	if(str2.size()<str1.size())
+		str2 = string(str1.size()-str2.size(), '0') + str2;
+
 	for (int i = str1.length() - 1; i >= 0; --i)
 	{
 		if (str1[i] + next >= str2[i])
@@ -21,13 +27,18 @@ void strMinus(string & str1, string & str2)
 			next = -1 + next / 10;
 		}
 	}
+
+	if(str2[0] == '0')
+		str2 = str2.substr(1, str2.size()-1);
 }
 
 bool strLarger(string & str1, string & str2)
 {
 	if (str1.size()>str2.size())
 		return true;
-	return str1>str2;
+	if (str1.size()<str2.size())
+		return false;
+	return str1>=str2;
 }
 
 int main(int argc, char const *argv[])
@@ -35,7 +46,7 @@ int main(int argc, char const *argv[])
 	string dividend, divisor;
 	int n;
 	vector<int> result;
-	int i;
+	int i,j;
 	int result_index;
 	string current;
 
@@ -49,6 +60,7 @@ int main(int argc, char const *argv[])
 
 		result.clear();
 
+		//delete proceeding 0s
 		for (i = 0; i<dividend.length(); ++i)
 		{
 			if (dividend[i] != '0')
@@ -66,52 +78,77 @@ int main(int argc, char const *argv[])
 			divisor = divisor.substr(i, divisor.length() - i);
 
 
+		//directly output result in this condicion
 		if (dividend.length()<divisor.length())
 		{
 			cout << 0 << endl;
 			continue;
 		}
 
+
+		/*cout<<"dividend: "<<dividend<<endl;
+		cout<<"divisor: "<<divisor<<endl;*/
+
 		result_index = divisor.length() - 1;
 		current = dividend.substr(0, result_index + 1);
 		while (result_index != dividend.length() - 1)
 		{
+			/*cout<<"current: "<<current<<" divisor: "<<divisor<<endl;*/
+
 			i = 0;
-			while (current >= divisor)
+			while (strLarger(current, divisor))
 			{
+				/*cout<<"In while: current: "<<current<<" divisor: "<<divisor<<endl;*/
 				strMinus(current, divisor);
+				for (j = 0; j<current.length(); ++j)
+				{
+					if (current[j] != '0')
+						break;
+				}
+				if (j != 0)
+					current = current.substr(j, current.length() - j);
 				++i;
 			}
 			result.push_back(i);
 
+			//delete all zeros in the front
 			for (i = 0; i<current.length(); ++i)
-			if (current[i] != '0')
-				break;
+			{
+				if (current[i] != '0')
+					break;
+			}
 			if (i != 0)
 				current = current.substr(i, current.length() - i);
 
-			while (strLarger(divisor, current))
+			if(current.empty())
 			{
-				result.push_back(0);
-				if(current.empty())
-					current.push_back(dividend[++result_index]);
-				else
-				{
-					current = current.substr(1, current.length() - 1);
-					current.push_back(dividend[++result_index]);
-				}				
+				if(dividend[++result_index] == '0')
+					continue;
+				current.push_back(dividend[result_index]);
+			}
+			else
+			{
+				current.push_back(dividend[++result_index]);
 			}
 
 		}
+
+
 		i = 0;
-		while (current >= divisor)
+		while (strLarger(current, divisor))
 		{
 			strMinus(current, divisor);
 			++i;
+			for (j = 0; j<current.length(); ++j)
+			{
+				if (current[j] != '0')
+					break;
+			}
+			if (j != 0)
+				current = current.substr(j, current.length() - j);
+
 		}
 		result.push_back(i);
-
-
 
 		for (i = 0; i<result.size(); ++i)
 		{
