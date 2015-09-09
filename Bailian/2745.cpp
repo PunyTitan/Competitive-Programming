fc39ed1fd79c58fba4189fa6e82bd8c6474d7c7f
PunyTitan <pinjiahe@gmail.com>
2015-09-09 09:24:37 +0800

    2745 draft

---
+++ Bailian/2745.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int s, t;
+	while(cin>>s>>t)
+	{
+		if(s==0 && t==0)
+			break;
+
+		
+	}
+	return 0;
+}
