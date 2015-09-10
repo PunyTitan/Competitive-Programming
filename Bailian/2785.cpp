e0cd1bf0576670e382ef2a74b59235d01fbbd950
PunyTitan <pinjiahe@gmail.com>
2015-09-10 20:48:01 +0800

    2785 draft

---
+++ Bailian/2785.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int zushu, N;
+	char menpai;
+
+	cin>>zushu;
+	while(zushu--!=0)
+	{
+		cin>>N;
+		while(cin>>menpai)
+		{
+			if(menpai == '0')
+				break;
+			
+		}
+	}
+	return 0;
+}
